046ed6851dc9ed412595a0449c943f5e5f60757d
valentine <valentine@HALL-9000>
2018-12-09 18:34:27 +0300

    Fix some logical mistakes in function names. Add new /temp command

--- bot.py
+++ bot.py
@@ -16,22 +16,23 @@ bot = telebot.TeleBot(config.token)
 helping_msg = "Hellow. I am HutWeatherBot and I can tell you about weather in you hut.\n Just press /give, when you want to know"
 stop_msg = "Ok, I'll stop"
 starting_msg = "Let's go, here your weather:"
+send_temp_msg = "Current temp is: "
 
 @bot.message_handler(commands=['help'])
 def send_help_msg(message):
     bot.send_message(message.chat.id, helping_msg)
     
 @bot.message_handler(commands=['give'])
-def starting_monitoring(message):
+def send_weather(message):
     bot.send_message(message.chat.id, starting_msg)
     answering_string = subprocess.check_output([r"/home/valentine/bosh_tph"])
     bot.send_message(message.chat.id, answering_string)
         
-@bot.message_handler(commands=['stop'])
-def stop_monitoring(message):
-    bot.send_message(message.chat.id, stop_msg)
-    flag_stop = 0
-
+@bot.message_handler(commands=['temp'])
+def send_temp(message):
+    bot.send_message(message.chat.id, starting_msg)
+    answering_string = subprocess.check_output([r"/home/valentine/bosh_tph"])
+    bot.send_message(message.chat.id, answering_string[0::answering_string.find("\n")])
     
 #exit(0)
 
