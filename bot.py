1379fb9e0c2c9db8670cb059e3d24211f65370bd
valentinnsh <valentinnsh@gmail.com>
2018-12-09 23:38:03 +0300

    Add working commands for measure only humidity,pressure or temerature.

--- bot.py
+++ bot.py
@@ -14,10 +14,12 @@ import time
 bot = telebot.TeleBot(config.token)
 
 
-helping_msg = "Hellow. I am HutWeatherBot and I can tell you about weather in you hut.\n Just press /give, when you want to know"
+helping_msg = "Hellow. I am HutWeatherBot and I can tell you about weather in you hut.\n Just press /give, when you temperature, pressure and humidity.\nSend /temp if you want to know just temperature\nSend /pres if you want to know just pressure\nSend /hum just for humidity\n\n Enjoy!"
 stop_msg = "Ok, I'll stop"
 starting_msg = "Let's go, here your weather:"
 send_temp_msg = "Current temp is: "
+send_pres_msg = "Current pressure is: "
+send_hum_msg = "Current humidity is: "
 
 @bot.message_handler(commands=['help'])
 def send_help_msg(message):
@@ -31,10 +33,27 @@ def send_weather(message):
         
 @bot.message_handler(commands=['temp'])
 def send_temp(message):
-    bot.send_message(message.chat.id, starting_msg)
+    bot.send_message(message.chat.id, send_temp_msg)
     answering_string = subprocess.check_output([r"/home/valentine/bosh_tph"])
-    bot.send_message(message.chat.id, answering_string[0::answering_string.find("\n")])
-    
+    pos = int(answering_string.find("\n".encode('utf8'), 0))
+    bot.send_message(message.chat.id, answering_string[0:pos])
+
+@bot.message_handler(commands=['pres'])
+def send_pres(message):
+    bot.send_message(message.chat.id, send_pres_msg)
+    answering_string = subprocess.check_output([r"/home/valentine/bosh_tph"])
+    pos1 = int(answering_string.find("\n".encode('utf8'), 0))
+    pos2 = int(answering_string.find("\n".encode('utf8'), pos1+1))
+    bot.send_message(message.chat.id, answering_string[pos1:pos2])
+
+@bot.message_handler(commands=['hum'])
+def send_hum(message):
+    bot.send_message(message.chat.id, send_hum_msg)
+    answering_string = subprocess.check_output([r"/home/valentine/bosh_tph"])
+    pos1 = int(answering_string.find("\n".encode('utf8'), 0))
+    pos2 = int(answering_string.find("\n".encode('utf8'), pos1+1))
+    pos3 = int(answering_string.find("\n".encode('utf8'), pos2+1))
+    bot.send_message(message.chat.id, answering_string[pos2:pos3])
 #exit(0)
 
 if __name__ == '__main__':
